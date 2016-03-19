b224cc9d98d051d310f5bbdb5f0ccee4b9b47208
sersajur <sersajur@gmail.com>
2016-03-19 22:11:35 +0200

    add smoothing stage before extrema extraction

--- src/FeatureExtraction.py
+++ src/FeatureExtraction.py
@@ -1,3 +1,6 @@
+from DataGetter import PatientGlucoseMeasurement
+import numpy
+
 class ExtremaFilter:
     @staticmethod
     def _Sign(i_val):
@@ -19,3 +22,37 @@ class ExtremaFilter:
         indexed_extremal_measurements = filter(lambda index_x: predicate_values[index_x[0]], enumerate(measurements[1:-1]))
         extremal_measurements = map(lambda x: x[1], indexed_extremal_measurements)
         return extremal_measurements
+
+
+class SeriesModifier:
+    @staticmethod
+    def Smooth(measurements, window_len=10, window='hanning'):
+        # get just glucose values
+        x = numpy.array(map(lambda m: m.GetGlucoseLevel(), measurements))
+
+        # code of smoothing from https://mail.scipy.org/pipermail/scipy-dev/2006-January/005182.html
+        if x.ndim != 1:
+            raise ValueError("smooth only accepts 1 dimension arrays.")
+
+        if x.size < window_len:
+            raise ValueError("Input vector needs to be bigger than window size.")
+
+        if window not in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+            raise ValueError("Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+
+        s = numpy.r_[2*x[0]-x[window_len:1:-1], x, 2*x[-1]-x[-1:-window_len:-1]]
+
+        if window == 'flat':
+            w = numpy.ones(window_len, 'd')
+        else:
+            w = eval('numpy.'+window+'(window_len)')
+
+        y = numpy.convolve(w/w.sum(), s, mode='same')
+        smoothed_gl = y[window_len-1:-window_len+1]
+
+        # update measurements measurements
+        result = map(lambda m, new_gl: PatientGlucoseMeasurement(i_pt_id=m.GetPtId(), i_datetime=m.GetDateTime(),
+                                                                 i_glucose_level=new_gl),
+                     measurements, smoothed_gl)
+        return result
+
--- src/Test.py
+++ src/Test.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from DataGetter import DataGetter
 from FeatureExtraction import ExtremaFilter
+from FeatureExtraction import SeriesModifier
 
 src_path = u'D:/GDrive/Диплом 2/DataPreparation/output/Patient#2/Period_from_2000-01-08__21-40.xlsx'
 
@@ -14,22 +15,29 @@ measurements = DataGetter.GetAllMeasurements(src_path,
                                              i_datetime_column_num=2,
                                              i_glucose_level_column_num=3)
 
-extremal_measurements = ExtremaFilter.ExtractExtremalMeasurements(measurements)
+smooth_measurements = SeriesModifier.Smooth(measurements)
+extremal_measurements = ExtremaFilter.ExtractExtremalMeasurements(smooth_measurements)
 
-datetime = np.array(map(lambda x: x.GetDateTime(), measurements))
+# original
 gl_level = np.array(map(lambda x: x.GetGlucoseLevel(), measurements))
+datetime = np.array(map(lambda x: x.GetDateTime(), measurements))
 
+# smoothed
+sm_gl_level = np.array(map(lambda x: x.GetGlucoseLevel(), smooth_measurements))
+
+# extrema
 ex_date_time = np.array(map(lambda x: x.GetDateTime(), extremal_measurements))
 ex_gl_level = np.array(map(lambda x: x.GetGlucoseLevel(), extremal_measurements))
 
-print '\n'.join(map(str, extremal_measurements))
-
+# plot settings
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.xaxis.set_major_locator(dates.DayLocator())
 dmyhm = dates.DateFormatter('%d.%m.%Y %H:%M')
 ax.xaxis.set_major_formatter(dmyhm)
 plt.xticks(rotation='vertical')
+# plot data
 plt.plot(datetime, gl_level, c='b')
+plt.plot(datetime, sm_gl_level, c='g')
 plt.plot(ex_date_time, ex_gl_level, 'ro')
 plt.show()
